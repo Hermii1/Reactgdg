98423e47ee02a0dda24798da03007cdc6a38e9e5
Hermii1 <hermelaejigu4@gmail.com>
2025-03-26 21:33:09 +0300

    Initial commit of my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Notes from './components/Notes';
+import TaskManager from './components/TaskManager';
 
 const App = () => {
     return (
         <div>
             <Notes />
+            <TaskManager />
         </div>
+        
     );
 };
+
+
 
 export default App;
